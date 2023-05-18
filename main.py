b3ce1fc6fa9a1af414ab7e02c2d794f7d98e1870
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-18 23:18:01 +0200

    created the canvas image grid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -8,8 +9,9 @@ from tkinter import *
 window = Tk()
 window.title("Password Manager")
 
-
-
-
+canvas = Canvas(width=200, height=200)
+image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=image)
+canvas.grid(column=1, row=1, padx=50, pady=50)
 
 window.mainloop()
