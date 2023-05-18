c652b83ace04f9268e954b99935115c7a7142a18
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-19 00:51:25 +0200

    creating the password generating function

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from tkinter import *
 from tkinter import messagebox
-
-
+from random import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+password_letters = [choice[letters] for _ in range(randint(8, 10))]
+password_symbols = [choice[symbols] for _ in range(randint(2, 4))]
+password_numbers = [choice[numbers] for _ in range(randint(2, 4))]
+
+password_list = password_numbers + password_symbols + password_letters
+
+shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_input.get()
