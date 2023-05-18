7ad1fdad086b58ac61519b3ea3c4acb72346fd0b
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-19 00:52:58 +0200

    refactoring the password generating function

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ password_list = password_numbers + password_symbols + password_letters
 
 shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+password = "".join(password_list)
+
 
 print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
