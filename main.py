afb11a73e8d77337a8340fca80c61f4f567bd7b7
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-19 00:55:47 +0200

    refactoring the password generating function

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
 from random import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-password_letters = [choice[letters] for _ in range(randint(8, 10))]
-password_symbols = [choice[symbols] for _ in range(randint(2, 4))]
-password_numbers = [choice[numbers] for _ in range(randint(2, 4))]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-password_list = password_numbers + password_symbols + password_letters
+    password_list = password_numbers + password_symbols + password_letters
 
-shuffle(password_list)
+    shuffle(password_list)
 
-password = "".join(password_list)
+    password = "".join(password_list)
+    password_input.insert(0,password)
 
 
-print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_input.get()
@@ -74,7 +79,7 @@ password_input = Entry(width=35)
 password_input.grid(column=1, row=3, columnspan=2)
 
 # The Button UI
-generate_button = Button(text="Generate Password", )
+generate_button = Button(text="Generate Password",command=generate_password )
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36, justify="center", command=save_password)
