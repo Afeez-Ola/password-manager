98cd9a1c0d6e114ae63645abb74a46d270ff6572
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-19 01:01:13 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
-from random import *
+from random import choice, randint
 import pyperclip
-
+import string
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    letters = string.ascii_letters
+    numbers = string.digits
+    symbols = string.punctuation
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
@@ -22,7 +20,8 @@ def generate_password():
 
     password = "".join(password_list)
     pyperclip.copy(password)
-    password_input.insert(0,password)
+    password_input.delete(0, tk.END)
+    password_input.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -37,54 +36,55 @@ def save_password():
         messagebox.showerror(title="Oops", message=f"Please don't leave any fields empty!")
     else:
         is_true = messagebox.askokcancel(title=website,
-                                         message=f"These are the details entered: \nEmail:{email}\nPassword:{password}")
+                                         message=f"These are the details entered:\nEmail: {email}\nPassword: {password}")
     if is_true:
         with open("data.txt", "a") as dataFile:
-            dataFile.writelines(data)
-        website_input.delete(0, END)
+            dataFile.write(data)
+        website_input.delete(0, tk.END)
+        id_input.delete(0, tk.END)
         id_input.insert(0, "@gmail.com")
-        password_input.delete(0, END)
+        password_input.delete(0, tk.END)
         website_input.focus_set()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = Tk()
+window = tk.Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200)
-image = PhotoImage(file="logo.png")
+canvas = tk.Canvas(width=200, height=200)
+image = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=image)
-canvas.grid(column=1, row=0, )
+canvas.grid(column=1, row=0)
 
 # The Label UI
-website_label = Label(text="Website:", font=("Arial", 12), )
+website_label = tk.Label(text="Website:", font=("Arial", 12))
 website_label.grid(column=0, row=1)
 
-id_label = Label(text="Email/Username:", font=("Arial", 12), pady=12, )
+id_label = tk.Label(text="Email/Username:", font=("Arial", 12), pady=12)
 id_label.grid(column=0, row=2)
 
-password_label = Label(text="Password:", font=("Arial", 12), padx=12, )
+password_label = tk.Label(text="Password:", font=("Arial", 12), padx=12)
 password_label.grid(column=0, row=3)
 
 # The input UI
-website_input = Entry(width=35)
+website_input = tk.Entry(width=35)
 website_input.focus_set()
 website_input.grid(column=1, row=1, columnspan=2)
 
-id_input = Entry(width=35)
+id_input = tk.Entry(width=35)
 id_input.insert(0, "@gmail.com")
 id_input.grid(column=1, row=2, columnspan=2)
 
-password_input = Entry(width=35)
+password_input = tk.Entry(width=35)
 password_input.grid(column=1, row=3, columnspan=2)
 
 # The Button UI
-generate_button = Button(text="Generate Password",command=generate_password )
+generate_button = tk.Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36, justify="center", command=save_password)
-add_button.grid(column=1, row=4, columnspan=3, pady=12, )
+add_button = tk.Button(text="Add", width=36, justify="center", command=save_password)
+add_button.grid(column=1, row=4, columnspan=3, pady=12)
 
 window.mainloop()
