362561ea926570231b68c8c376ef4755f7c84905
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-18 22:43:50 +0200

    Setting up the tkinter window

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+
+
+
+
+
+window.mainloop()
