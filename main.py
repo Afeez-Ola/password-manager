979d61eb9fc4873fb9757ddc8870fb0966b2d5ba
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-05-18 23:28:34 +0200

    Added the email/username label

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=image)
 canvas.grid(column=1, row=0, padx=50, pady=50)
 
-website_label = Label(text="Website:",font=("Arial", 18,'bold'))
+website_label = Label(text="Website:",font=("Arial", 12))
 website_label.grid(column=0,row=1)
+
+id_label = Label(text="Email/Username:",font=("Arial", 12),padx=12,pady=12,)
+id_label.grid(column=0,row=2)
 
 window.mainloop()
